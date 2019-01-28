4099c00ea4edb509fa1f69060142d1c1b0160a2c
181184muhammadRahim <46868114+181184muhammadRahim@users.noreply.github.com>
2019-01-28 10:55:54 +0500

    Add files via upload

---
+++ q2_181184.h
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+#include<string>
+struct gitHubuser
+{
+	string firstname;
+	string username;
+	string email;
+	int foldercount;
+	string *githubfolders;
+
+};
+void readdatafromfile(gitHubuser* users, string filepath)
+{
+
+}
